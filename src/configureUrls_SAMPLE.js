ff56d3ffeb21d36818b78ef0a9aa9f5d7046f5dd
Benjamin Lofo Follo <benjamin.lofofollo@hotmail.com>
2018-10-09 19:58:23 -0400

    Use Configure Urls Globally

--- src/configureUrls_SAMPLE.js
+++ src/configureUrls_SAMPLE.js
@@ -1,13 +1,14 @@
 let baseUrl = 'https://absurd-pizzas-9864.twil.io/'
 
 module.exports = {
-  baseUrl: baseUrl,
-  taskRouterToken: baseUrl + 'taskrouter-client-token',
-  clientToken: baseUrl + 'twilio-client-token',
-  conferenceTerminate: baseUrl + 'terminate-conference',
-  internalTransfer: baseUrl + 'internal-transfer',
-  conferenceEvents: baseUrl + 'conference-event',
-  callHold: baseUrl + 'hold-call',
-  callOutbound: baseUrl + 'outbound',
-  callOutboundCallback: baseUrl + 'outbound-call-callback',
+    baseUrl: baseUrl,
+    taskRouterToken: baseUrl + 'taskrouter-client-token',
+    clientToken: baseUrl + 'twilio-client-token',
+    conferenceTerminate: baseUrl + 'terminate-conference',
+    conferenceEvents: baseUrl + 'conference-events',
+    taskRouterEvents: baseUrl + 'taskrouter-events',
+    callHold: baseUrl + 'hold-call',
+    callOutbound: baseUrl + 'outbound',
+    callOutboundCallback: baseUrl + 'outbound-callback',
+    internalTransferCallback: baseUrl + 'internal-transfer-callback',
 }
